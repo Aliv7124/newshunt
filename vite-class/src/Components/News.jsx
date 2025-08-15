aff1e0258d683d3d49b2ff30007306370122d1b9
Aliv7124 <alivroy25@gmail.com>
2025-08-15 18:43:37 +0530

    add backend

--- vite-class/src/Components/News.jsx
+++ vite-class/src/Components/News.jsx
@@ -1,114 +1,96 @@
-import React, { Component } from 'react'
-import Newsitem from './Newsitem'
-import Spinner from '../Spinner';
-import PropTypes from "prop-types";
+import React, { Component } from 'react';
+import NewsItem from './NewsItem';
+import Spinner from "../Spinner"
+import PropTypes from 'prop-types';
+
 export class News extends Component {
+    static defaultProps = {
+        category: 'general',
+        pagesize: 8
+    }
 
-static defaultProps = {
-  pagesize: 8,
-  category: "sports"
-};
+    static propTypes = {
+        category: PropTypes.string,
+        pagesize: PropTypes.number
+    }
 
-static propTypes = {
-  category: PropTypes.string,
-  pagesize: PropTypes.number
-};
+    constructor(props) {
+        super(props);
+        this.state = {
+            articles: [],
+            loading: false,
+            page: 1
+        };
+    }
 
+      async fetchNews(page, category) {
+        this.setState({ loading: true });
+        let url = `https://newshunt-backend.onrender.com/news?category=${category}&page=${page}&pagesize=${this.props.pagesize}`;
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        this.setState({
+            articles: parsedData.articles || [],
+            loading: false,
+            page: page
+        });
+    }
 
+    async componentDidMount() {
+        this.fetchNews(1, this.props.category);
+    }
 
+    async componentDidUpdate(prevProps) {
+        // If category changes, reload news
+        if (prevProps.category !== this.props.category) {
+            this.fetchNews(1, this.props.category);
+        }
+    }
 
+    handleNextClick = async () => {
+        this.fetchNews(this.state.page + 1, this.props.category);
+    }
 
+    handlePrevClick = async () => {
+        if (this.state.page > 1) {
+            this.fetchNews(this.state.page - 1, this.props.category);
+        }
+    }
 
-next=async()=>{
-  this.props.setProgress(0);
- let url=`https://newsapi.org/v2/top-headlines?category=${this.props.category}&apiKey=fdce84e149f34c77ac57b682efd25052&page=${this.state.page+1}&pagesize=${this.props.pagesize}`;
- this.setState({loader:true})
-let data=await fetch(url);
-let parsedData=await data.json()
-console.log(parsedData);
-this.setState({
-  page:this.state.page+1,
-  articles:parsedData.articles,
-loader:false,
-})
-this.props.setProgress(100);
+    render() {
+        return (
+            <div className="container my-3">
+                <h1 className="text-center" style={{ margin: '35px 0px' }}>
+                    NewsHunt - Top {this.props.category} Headlines
+                </h1>
+                {this.state.loading && <Spinner />}
+                <div className="row">
+                    {!this.state.loading && this.state.articles.map((element) => {
+                        return (
+                            <div className="col-md-4" key={element.url}>
+                                <NewsItem
+                                    title={element.title ? element.title : ""}
+                                    description={element.description ? element.description : ""}
+                                    imageUrl={element.urlToImage}
+                                    newsUrl={element.url}
+                                    author={element.author}
+                                    date={element.publishedAt}
+                                    source={element.source.name}
+                                />
+                            </div>
+                        );
+                    })}
+                </div>
+                <div className="container d-flex justify-content-between">
+                    <button disabled={this.state.page <= 1} className="btn btn-dark" onClick={this.handlePrevClick}>
+                        &larr; Previous
+                    </button>
+                    <button className="btn btn-dark" onClick={this.handleNextClick}>
+                        Next &rarr;
+                    </button>
+                </div>
+            </div>
+        );
+    }
 }
 
-
-prev=async()=>{
-  this.props.setProgress(0);
-  let url=`https://newsapi.org/v2/top-headlines?category=${this.props.category}&apiKey=fdce84e149f34c77ac57b682efd25052&page=${this.state.page-1}&pagesize=${this.props.pagesize}`;
-  this.setState({loader:true})
-let data=await fetch(url);
-let parsedData=await data.json()
-console.log(parsedData);
-this.setState({
-  page:this.state.page-1,
-  articles:parsedData.articles,
-  loader:false,
-  })
-  this.props.setProgress(100);
-}
-
-constructor(){
-  super();
-  this.state={
- articles:[],
- loader:false,
- page:1
-  }
-}
-
-async componentDidMount(){
-  this.props.setProgress(0);
-let url=`https://newsapi.org/v2/top-headlines?category=${this.props.category}&apiKey=fdce84e149f34c77ac57b682efd25052&page=1&pagesize=${this.props.pagesize}`;
-this.setState({loader:true})
-let data=await fetch(url);
-let parsedData=await data.json()
-console.log(parsedData);
-this.setState({articles:parsedData.articles,
-loader:false,
-})
-this.props.setProgress(100);
-}
-async componentDidUpdate(prevProps) {
-  if (prevProps.category !== this.props.category) {
-    this.props.setProgress(0);
-    let url = `https://newsapi.org/v2/top-headlines?category=${this.props.category}&apiKey=fdce84e149f34c77ac57b682efd25052&page=1&pagesize=${this.props.pagesize}`;
-    this.setState({ loader: true, page: 1 });  
-    let data = await fetch(url);
-    let parsedData = await data.json();
-    this.setState({
-      articles: parsedData.articles,
-      loader: false
-    });
-    this.props.setProgress(100);
-  }
-}
-
-  render() {
-    return (
-        <div className="container my-3">
-          <h1 className="text-center">Newshunt-Top Headlines</h1>
-         {this.state.loader&&<Spinner/>}
-         <div className="row my-3">
-          {!this.state.loader&& this.state.articles.map((element)=>{
-           return  <div className="col md-4" key={element.url}>
-         <Newsitem  title={element.title} description={element.description?element.description:""} date={element.publishedAt} imageUrl={element.urlToImage} newsUrl={element.url}/>
-        </div>
-       })}
-         </div>
-         <div className="container d-flex justify-content-between">
-         <button disabled={this.state.page<=1} type="button" className="btn btn-primary" onClick={this.prev}>&larr;Previuos</button>
-         <button type="button" className="btn btn-primary" onClick={this.next}>Next&rarr;</button>
-         </div>
-        </div>
-   
-    )
-  }
-}
-
-export default News
-
-
-
+export default News;
