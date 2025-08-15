fac60f5e1ec8077a2d6b26982b62b95150e8f598
Aliv7124 <alivroy25@gmail.com>
2025-08-15 17:44:11 +0530

    changes

--- vite-class/src/main.jsx
+++ vite-class/src/main.jsx
@@ -4,7 +4,7 @@ import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/vite-class/"> 
+  <BrowserRouter basename="/newshunt/"> 
     <App />
   </BrowserRouter>
 )
